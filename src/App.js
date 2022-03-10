39edf7eaba8943c07eb146bdac5b92a2d625dc65
jinjoo <wlswnzz15@gmail.com>
2022-03-10 16:03:00 +0900

    plus0310

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
 import Pizza from './components/Pizza';
-import Special from './components/Special';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 const App = () => {
-  let [count,setcount] = useState(0);
   let [pizza,setpizza] = useState(false);
-
-  const clickHandler = () => {
-    setcount(++count);
-    setpizza(!pizza)
-  }
-
 
   return(
     <>
-      <Button count={count} onClick={clickHandler} />
-      {pizza? <Pizza /> : null}
-      <Pizza/>
-
-      <Pizza/>
-      <Special/>
+    <Header/>
+      <Pizza />
+    <Footer/> 
     </>
       )
     }
 
-    const Button = (props) => {
-      // props = {count:0 , onClick:clickHandler}
-      return(
-        <>
-          <button onClick ={props.onClick}>🍔</button>
-          <span > clicked : {props.count}</span>
-        </>
-      )
+ 
   
 
 
-}
 
 export default App;
---
+++ src/components/NewPizzaList.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import Card from './Card'
+import NEW from './NEW'
+import classes from './NewPizzaList.module.css'
+
+const NEWPIZZA = [
+  {
+    id: "pizza1",
+    name: "아메리칸 패티 멜트",
+    description:
+      "#그릴드 직화 패티+치즈 멜트의 운명적 맛남!",
+    Lprice: 28900,
+    Mprice: 22000,
+    src : require("../images/newpizza1.jpg")
+
+  },
+  {
+    id: "pizza2",
+    name: "아메리칸 패티 멜트+블랙앵거스 더블치즈엣지",
+    description:
+      "#패티 멜트와 스테이크의 육즙 가득한 만남!!",
+    Lprice: 36900,
+    Mprice: 28500,
+    src : require("../images/newpizza2.jpg")
+  },    
+  {
+    id: "pizza3",
+    name: "아메리칸 패티 멜트+뉴욕 오리진 더블치즈엣지",
+    description:
+      "#아메리칸 패티 멜트와 미국 피자 오리진의 만남!",
+    Lprice: 33900,
+    Mprice: 25000,
+    src : require("../images/newpizza3.jpg")
+  },
+]
+
+
+const NewPizzaList = () => {
+
+    const [newPizza, setNewPizza] = useState(NEWPIZZA);
+    console.log(newPizza);
+
+    const newPizzaList = newPizza.map((pizza) => 
+      <NEW
+        key = {pizza.id}
+        name = {pizza.name}
+        description = {pizza.description}
+        Lprice = {pizza.Lprice}
+        Mprice = {pizza.Mprice}
+        src = {pizza.src}
+        />
+    )
+    console.log(newPizzaList);
+
+    return(
+      <section className={classes.pizza}>
+        <Card >
+          <ul className={classes.card}>{newPizzaList}</ul>
+        </Card>
+      </section>
+    )
+
+  }
+
+
+export default NewPizzaList
